221d6bc59d3855035b31e139479fd06589edbf8a
Paweł Kalicki <kalicki.paul@icloud.com>
2020-03-31 09:58:21 +0200

    Added load const from files

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+lib_folder = Path("../libs/")
+
+constFiles = {
+    0: 'LIBRARIES',
+    1: 'NAMESPACES',
+    2: 'KEYWORDS',
+    3: 'FUNCTIONS',
+    4: 'TYPESVARIABLES',
+    5: 'VARIABLES',
+    6: 'VALUES'
+}
+resources = {}
+
+for i in constFiles.values():
+    with open('{0}/{1}.txt'.format(lib_folder, i), 'r') as f:
+        resources[i] = f.read().splitlines()
+
+libraries = resources[constFiles[0]]
+namespaces = resources[constFiles[1]]
+keyWords = resources[constFiles[2]]
+functions = resources[constFiles[3]]
+typesVariables = resources[constFiles[4]]
+variables = resources[constFiles[5]]
+values = resources[constFiles[6]]
